3b25c8921758d668d05a3c7806b3fb7a9fe29e11
pratiktariya <pratiktariya000@gmail.com>
2025-12-26 22:28:29 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // TODO: Replace with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyCWUMSK0JDPSsPKi_hNh7A-LMIgNKrSfDc",
+  authDomain: "ecommerce-293a9.firebaseapp.com",
+  projectId: "ecommerce-293a9",
+  storageBucket: "ecommerce-293a9.firebasestorage.app",
+  messagingSenderId: "717517812738",
+  appId: "1:717517812738:web:26cecf68bc1e261b2e631b",
+  measurementId: "G-CESMPZ623W"
 };
 
 const app = initializeApp(firebaseConfig);
